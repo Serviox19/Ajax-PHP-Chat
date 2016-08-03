0313066bced38622acaa9227a5b3fd6e540aeadd
Servio Mora <servio.mora19@gmail.com>
2016-08-03 16:26:42 -0400

    created database and html template to start

--- index.php
+++ index.php
@@ -1,10 +1,33 @@
-<?php
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ajax Chat</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="js/script.js"></script>
+  </head>
+  <body>
 
+    <div class="container">
+      <header>
+        <h1>Chat Box</h1>
+      </header>
+      <div id="chats">
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+      <div id="newChat">
+        <form class="" action="index.html" method="post">
+          <label>Name: </label>
+          <input type="text" id="name">
+          <label>Message Text: </label>
+          <input type="text" id="chats">
+          <input type="submit" id="submit" value="Chat">
+        </form>
+      </div>
+    </div>
 
-  function sayIt($words){
-    echo $words;
-  }
-
-  sayIt("Hello!");
-
-?>
+  </body>
+</html>
